aed411d03d8730c123d9c8183832d49f9bc36bda
Michael Girdley <michael@Michaels-MacBook-Pro-2.local>
2013-04-07 20:40:44 -0500

    Changed num of pages to get

--- leaderboard/get-all-scores.sh
+++ leaderboard/get-all-scores.sh
@@ -1,6 +1,6 @@
-python ./get-scores.py 1 1 1030 &
-python ./get-scores.py 2 1 700 &
-python ./get-scores.py 3 1 60 &
+python ./get-scores.py 1 1 881 &
+python ./get-scores.py 2 1 552 &
+python ./get-scores.py 3 1 43 &
 python ./get-scores.py 4 1 36 &
 python ./get-scores.py 5 1 25 &
 python ./get-scores.py 6 1 17 &
